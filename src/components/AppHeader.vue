7ef60eda8e41021601aac293d2cefb65a4a8165c
Simone Murri <simonemurri701@gmail.com>
2024-01-09 11:19:50 +0100

    inserito header, span ed icone in between e inserita immagine

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,15 @@
 <template lang="">
-    <div>
-        <h1>Header</h1>
+    <div class="header">
+        <span>Avada e associates legal team announce deal to open up oil & gas</span>
+        <span>
+            <i class="fa-brands fa-square-facebook"></i>
+            <i class="fa-brands fa-square-twitter"></i>
+            <i class="fa-brands fa-square-instagram"></i>
+            <i class="fa-brands fa-square-youtube"></i>
+        </span>    
+    </div>
+    <div class="header-image-container">
+        <img class="header-image" src="../assets/images/video_law_home_preview.jpg" alt="">
     </div>
 </template>
 <script>
@@ -8,6 +17,32 @@ export default {
     
 }
 </script>
-<style lang="">
-    
+<style lang="scss" scoped>
+
+.header-image-container{
+    max-width: 100%;
+    height: 400px;
+    overflow: hidden;
+}
+
+.header-image {
+    width: 100%;
+    height: auto;
+    display: block;
+}
+.header{
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    align-items: center;
+    padding: 0rem 20rem;
+    font-size: 12px;
+    margin: .5rem 0rem;
+}
+
+.fa-brands {
+    color: #55A098;
+    margin-right: .25rem;
+    font-size: 20px;
+}
 </style>
